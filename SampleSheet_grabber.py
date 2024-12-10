e680979525c81859a4176d6ec4f2dd48a07ef0b1
Abigail Hicks <Abigail.Hicks@doh.wa.gov>
2024-12-10 14:54:06 -0800

    Update SampleSheet_grabber.py

--- SampleSheet_grabber.py
+++ SampleSheet_grabber.py
@@ -6,3 +6,6 @@
 # Can filter by date, project, etc. 
     # Then grab list of all id/name of runs that match criteria (can export filtered data to file, maybe dataframe?)
     # Then use that to grab sample sheet (for loop?)
+
+# Example of basespace CLI code to get information I need
+# $ bs list datasets -F Project.Name -F AppSession.Name -F AppSession.DateCompleted --filter-field=Project.Name --filter-term=WaitingforQC
