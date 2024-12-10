a8346037e9e9a07e5f18fc24c16b98b479facf5d
Abigail Hicks <Abigail.Hicks@doh.wa.gov>
2024-12-10 14:44:58 -0800

    Create SampleSheet_grabber.py
    Setting up a python file and establishing an outline

---
+++ SampleSheet_grabber.py
@@ -0,0 +1,8 @@
+# Avenue for no matches found
+
+# Make arguments for different inputs
+    # Make default of time input the most recent run
+
+# Can filter by date, project, etc. 
+    # Then grab list of all id/name of runs that match criteria (can export filtered data to file, maybe dataframe?)
+    # Then use that to grab sample sheet (for loop?)
